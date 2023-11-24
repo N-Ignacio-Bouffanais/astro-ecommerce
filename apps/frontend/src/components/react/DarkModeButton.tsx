fac8fc4ca0739a72658291f894d573261a0c1850
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-11-24 16:07:19 -0300

    dark mode togle button on navbar works perfect

---
+++ apps/frontend/src/components/react/DarkModeButton.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+const DarkModeButton = () => {
+
+  const [theme, setTheme] = useState(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    }
+    return "light";
+  });
+
+  useEffect(() => {
+    const doc = document.querySelector("html") as HTMLElement;
+    if (theme === "dark") {
+      doc.classList.add("dark");
+    } else {
+      doc.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const HandleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <button onClick={HandleTheme}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M12 3h.393a7.5 7.5 0 0 0 7.92 12.446A9 9 0 1 1 12 2.992z"
+        />
+      </svg>
+    </button>
+  );
+};
+
+export default DarkModeButton;
