d585e8a7a719dee36ded76ab506b2f02d3ef5040
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-12-01 17:02:08 -0300

    avatar hover for signout and config

--- apps/frontend/src/components/react/DarkModeButton.tsx
+++ apps/frontend/src/components/react/DarkModeButton.tsx
@@ -26,9 +26,9 @@ const DarkModeButton = () => {
     <button onClick={HandleTheme}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="28"
-        height="28"
-        viewBox="0 0 28 28"
+        width="32"
+        height="32"
+        viewBox="0 0 32 32"
       >
         <path
           fill="none"
