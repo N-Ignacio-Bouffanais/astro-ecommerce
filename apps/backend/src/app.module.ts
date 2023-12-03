88ceea30896554644894a6d5a8cfabac5523b168
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-12-02 22:51:58 -0300

    drizzle module and service generate

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { DrizzleModule } from './drizzle/drizzle.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../../', 'frontend/dist'),
     }),
+    DrizzleModule,
   ],
   controllers: [AppController],
   providers: [AppService],
