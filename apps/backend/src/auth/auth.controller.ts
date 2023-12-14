f146036080202c3628856d4349cf18961de75486
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-12-13 23:36:05 -0300

    nanostores install, pages and navbar links

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -27,4 +27,8 @@ export class AuthController {
       throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
+
+  async SignUp(registerDto: RegisterDto){
+
+  }
 }
--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -7,7 +7,25 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private jwtService: JwtService, private config: ConfigService) {}
+  constructor(private db:DatabaseService, private jwtService: JwtService, private config: ConfigService) {}
 
   async SignIn(loginDto: LoginDto) {}
+
+  async SignUp(registerDto: RegisterDto) {
+    const findUser = await this.db
+    if (findUser) throw new Error(`User already registered`);
+
+    if (!findUser) {
+      const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+      const newUser = await this.
+
+      const user = {
+        firstname: newUser.firstname,
+      };
+
+      return `User ${user.} created successfully`;
+    }
+
+    throw new Error('This User already exists, Please create a new one');
+  }
 }
